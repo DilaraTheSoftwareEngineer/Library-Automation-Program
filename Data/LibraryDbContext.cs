f67c48f9ff1a8b3e7a9b1873a2548d72144c2d15
Dilara Hatunoğlu <dilarahatunoglu.34be@gmail.com>
2026-03-11 10:35:16 +0300

    The first files uploaded.

---
+++ Data/LibraryDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LibraryAutomation.Models; // Modellerinin bulunduğu namespace
+using System;
+
+namespace LibraryAutomation.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        // Tabloların tanımlanması
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+              string connectionString = "server=127.0.0.1;port=3307;database=librarydb;user=root;password=;";
+
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // İlişkileri burada netleştirebilirsin (Opsiyonel ama önerilir)
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Author)
+                .WithMany()
+                .HasForeignKey(b => b.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Genre)
+                .WithMany()
+                .HasForeignKey(b => b.GenreId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Models/Models.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryAutomation.Models
+{
+    public class User
+    {
+        [Key] public int Id { get; set; }
+        public string Username { get; set; }
+       public string Status { get; set; } = "Aktif"; // Aktif veya Banlı
+        public bool IsBanned { get; set; } = false;
+
+    }
+
+    [Table("Authors")]
+    public class Author
+    {
+        [Key] public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+
+    public class Genre
+    {
+        [Key] public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Book
+    {
+
+        [Key] public int Id { get; set; }
+        public bool IsFavorite { get; set; }
+        public bool IsExtended { get; set; } = false;
+        public bool WasBorrowedBefore { get; set; }
+        public DateTime? BorrowDate { get; set; } 
+        public string ShelfInfo { get; set; } 
+        public string AISummary { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+        public int GenreId { get; set; }
+        public virtual Genre Genre { get; set; }
+        public string Description { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public string ImageUrl { get; set; }
+        public bool IsBorrowed { get; internal set; }
+        public string BorrowedBy { get; internal set; }
+    }
+    public class BorrowRecord
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime ReturnDate { get; set; } 
+        public bool IsReturned { get; set; }
+    }
+}

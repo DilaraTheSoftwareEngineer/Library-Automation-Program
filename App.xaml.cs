8cfd589a1fa12432623a43ddbd8a8c64b95e1f10
Dilara Hatunoğlu <dilarahatunoglu.34be@gmail.com>
2026-03-11 10:39:10 +0300

    Apps

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace LibraryAutomationn
+{
+    public partial class App : Application
+    {
+        // Uygulama genelinde bir hata olursa yakalamak için burayı kullanabiliriz
+    }
+}
